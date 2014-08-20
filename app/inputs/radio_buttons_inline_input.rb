e72f51b29ea55b167ddf5d1ba864d57ea4ef6422
PerseP <persep@gmail.com>
2014-08-20 09:24:03 +0200

    Complete form

--- app/inputs/radio_buttons_inline_input.rb
+++ app/inputs/radio_buttons_inline_input.rb
@@ -1,7 +1,9 @@
 class RadioButtonsInlineInput < SimpleForm::Inputs::CollectionRadioButtonsInput
-  def input
+  def input(wrapper_options = nil)
+
     label_method, value_method = detect_collection_methods
 
+    # def collection_radio_buttons(method, collection, value_method, text_method, options = {}, html_options = {}, &block)
     @builder.send("collection_radio_buttons",
       attribute_name, collection, value_method, label_method,
       input_options, input_html_options, &collection_block_for_nested_boolean_style
